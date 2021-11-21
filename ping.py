5215c5c5d9571e326dca0ebaa7a6fc22e5ed748d
саша <94283721+svinobass@users.noreply.github.com>
2021-11-21 12:43:48 +0300

    Update ping.py
    Добавил движение мяча и столкновения.

--- ping.py
+++ ping.py
@@ -6,7 +6,6 @@ window = display.set_mode((1280, 720))
 display.set_caption(".")
 background = transform.scale(image.load("d.png"), (1280, 720))
 
-
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, playerx_size, playery_size, player_x, player_y, player_speed):
         super().__init__()
@@ -32,11 +31,15 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 500:
             self.rect.y += self.speed
     
-
 player1 = Player("pong.png", 30, 200, 50, 250, 15)
 player2 = Player("pong.png", 30, 200, 1200, 250, 15)
+ball = GameSprite("ball.png", 80, 80, 560, 280, 1)
+
+speed_x = 10
+speed_y = 10
 
 game = True
+finish = False
 while game:
     clock = time.Clock()
     clock.tick(FPS)
@@ -44,14 +47,16 @@ while game:
     for i in event.get():
         if i.type == QUIT:
             game = False
-        elif i.type == KEYDOWN:
-            if i.key == K_SPACE:
-                if num_fire < 5 and rel_time == False:
-                    player.fire()
-                    num_fire = num_fire + 1
-                if num_fire >= 5 and rel_time == False:
-                    last_time = timer()
-                    rel_time = True
+        
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+    if ball.rect.y > 640 or ball.rect.y < 0:
+        speed_y *= -1
+
+    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
 
     window.blit(background, (0, 0))
    
@@ -60,5 +65,6 @@ while game:
 
     player1.reset()
     player2.reset()
+    ball.reset()
 
     display.update()
